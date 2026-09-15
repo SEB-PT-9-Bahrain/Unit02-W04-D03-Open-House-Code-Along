dd81cc52e7f274fec8cbdfd041aa7e6ddd782ba8
omarakamal <omaraakamal@gmail.com>
2026-09-15 18:42:14 +0300

    update

--- routes/listings.routes.js
+++ routes/listings.routes.js
@@ -1,8 +1,26 @@
 const router = require("express").Router()
+const Listing = require('../models/Listing')
+const isSignedIn = require('../middleware/is-signed-in')
 
-router.get('/new',(req,res)=>{
+router.get('/new', isSignedIn,(req,res)=>{
     res.render('create-listing.ejs')
 })
 
+router.post('/', async (req,res)=>{
+    const createdListing = await Listing.create({
+        streetAddress: req.body.streetAddress,
+        city: req.body.city,
+        price: req.body.price,
+        size: req.body.size,
+        owner: req.session.user._id
+    })
+    res.redirect('/listings')
+})
+
+
+router.get('/', async (req,res)=>{
+    const listings = await Listing.find()
+    res.render('all-listings.ejs',{listings: listings})
+})
 
 module.exports = router;
